7f7ae6ba8b2829cc4c3f4ad0fa5454632253c26f
Yura <mypost@postsrv.com>
2019-04-08 13:21:16 +0300

    added href to prod list

--- application/views/products/showAll.php
+++ application/views/products/showAll.php
@@ -8,10 +8,10 @@ echo '<div class="prod_table">';
 
 foreach($products as $prod){
 	echo '<div class="prod_wrap">';		
-		echo '<div class="prod_name">'.$prod['name'].'</div>';
+		echo '<div class="prod_name"><a href="http://'.$_SERVER['SERVER_NAME'].'/product/show/'.$prod['id'].'">'.$prod['name'].'</a></div>';
 		echo '<div class="prod_price">Price: '.$prod['price'].'$</div>';
 		echo '<div class="prod_desc">Description: '.$prod['description'].'</div>';
-		echo '<a href="">Edit</a>';
+		echo '<a href="http://'.$_SERVER['SERVER_NAME'].'/product/edit/'.$prod['id'].'">Edit</a>';
 	echo '</div>';
 }
 
